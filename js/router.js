868b761273c96c07ea37009f0fcee03b853db242
rehan raihan <rehanurraihan@gmail.com>
2022-03-31 04:30:53 -0700

    Second commit

--- js/router.js
+++ js/router.js
@@ -3,24 +3,23 @@ const route = (event) => {
     event.preventDefault();
     window.history.pushState({}, "", event.target.href);
     handleLocation();
-};
-
-const routes = {
-    404: "./pages/404.html",
-    "/": "./pages/index.html",
-    "/getting-started": "./pages/getting-started.html",
-    "/components": "./pages/components.html",
-    // "/lorem": "./pages/lorem.html",
-};
-
-const handleLocation = async () => {
+  };
+  
+  const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/index.html",
+    "/about": "/pages/getting-started.html",
+    "/lorem": "/pages/components.html"
+  };
+  
+  const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
-    document.getElementById("app").innerHTML = html;
-};
-
-window.onpopstate = handleLocation;
-window.route = route;
-
-handleLocation();
+    document.getElementById("main-page").innerHTML = html;
+  };
+  
+  window.onpopstate = handleLocation;
+  window.route = route;
+  
+  handleLocation();
