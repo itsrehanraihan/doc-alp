d979bdc07e0910ef16cb5856009f8a41e310225d
rehan raihan <rehanurraihan@gmail.com>
2022-08-01 07:32:30 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-function darkTheme() {
-    // "use strict";
-    //Dark Mode
-    const toggleSwitch = document.getElementById('checkbox2');
-    const currentTheme = localStorage.getItem('class');
-
-    if (currentTheme) {
-        document.body.setAttribute('class', currentTheme);
-        
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-        }
-    }  
-
-    function switchTheme(e) {
-        if (e.target.checked) {
-            document.body.setAttribute('class', 'dark');
-            localStorage.setItem('class', 'dark');  
-        } else {
-            document.body.setAttribute('class', 'light');
-            localStorage.setItem('class', 'light'); 
-        }
-    } 
-
-    toggleSwitch.addEventListener('change', switchTheme, false);
+let theme = localStorage.getItem('data-theme');
+const checkbox = document.getElementById("switch");
+const changeThemeToDark = () =>{
+    document.documentElement.setAttribute("data-theme", "dark")
+    localStorage.setItem("data-theme", "dark")
+    console.log("I give you dark")
 }
+
+const changeThemeToLight = () =>{
+    document.documentElement.setAttribute("data-theme", "light")
+    localStorage.setItem("data-theme", 'light')
+    console.log("I give you light")
+}
+
+if(theme === 'dark'){
+    changeThemeToDark()
+}
+
+checkbox.addEventListener('change', ()=> {
+    let theme = localStorage.getItem('data-theme');
+    if (theme ==='dark'){
+        changeThemeToLight()
+        document.querySelector('.op-sun').innerHTML = 'Light <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/></svg>';
+    }else{
+        changeThemeToDark()
+        document.querySelector('.op-sun').innerHTML = 'Dark <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z"/></svg>';
+    }
+});
